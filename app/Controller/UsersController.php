db9021f00c3c4c14c9dbd08519de5ff1b05147bd
Thanos Demertzis <thandem88@gmail.com>
2012-01-12 16:00:22 +0200

    Create UsersController

---
+++ app/Controller/UsersController.php
@@ -0,0 +1,19 @@
+<?
+
+class UsersController extends AppController {
+
+    function login() {
+
+        if( $this->Auth->login() ) {
+
+            return $this->redirect( $this->Auth->redirect() );
+        } else {
+
+            $this->Session->setFlash(__("Δώστε έγκυρο όνομα και κωδικό χρήστη") );  
+        }
+    }
+
+    function logout() {
+
+    }
+}

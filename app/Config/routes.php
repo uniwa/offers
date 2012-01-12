78e2263739ea353fbf44544a6629445d2f54a81b
Thanasis Karetsos <karetsosjr@gmail.com>
2012-01-12 14:17:50 +0200

    fix routes.php
    - i cannot understand or imagine how this happened

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -1,6 +1,10 @@
 <?php
 /**
- * Default routes that CakePHP provides as catch all routes
+ * Routes configuration
+ *
+ * In this file, you set up routes to your controllers and their actions.
+ * Routes are very important mechanism that allows you to freely connect
+ * different urls to chosen controllers and their actions (functions).
  *
  * PHP 5
  *
@@ -12,75 +16,29 @@
  *
  * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @package       Cake.Config
- * @since         CakePHP(tm) v 2.0
+ * @package       app.Config
+ * @since         CakePHP(tm) v 0.2.9
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
+/**
+ * Here, we are connecting '/' (base path) to controller called 'Pages',
+ * its action called 'display', and we pass a param to select the view file
+ * to use (in this case, /app/View/Pages/home.ctp)...
+ */
+	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+/**
+ * ...and connect the rest of 'Pages' controller's urls.
+ */
+	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
 /**
- * Connects the default, built-in routes, including prefix and plugin routes. The following routes are created
- * in the order below:
- *
- * For each of the Routing.prefixes the following routes are created. Routes containing `:plugin` are only
- * created when your application has one or more plugins.
- *
- * - `/:prefix/:plugin` a plugin shortcut route.
- * - `/:prefix/:plugin/:action/*` a plugin shortcut route.
- * - `/:prefix/:plugin/:controller`
- * - `/:prefix/:plugin/:controller/:action/*`
- * - `/:prefix/:controller`
- * - `/:prefix/:controller/:action/*`
- *
- * If plugins are found in your application the following routes are created:
- *
- * - `/:plugin` a plugin shortcut route.
- * - `/:plugin/:action/*` a plugin shortcut route.
- * - `/:plugin/:controller`
- * - `/:plugin/:controller/:action/*`
- *
- * And lastly the following catch-all routes are connected.
- *
- * - `/:controller'
- * - `/:controller/:action/*'
- *
- * You can disable the connection of default routes by deleting the require inside APP/Config/routes.php.
+ * Load all plugin routes.  See the CakePlugin documentation on 
+ * how to customize the loading of plugin routes.
  */
-	$prefixes = Router::prefixes();
+	CakePlugin::routes();
 
-	if ($plugins = CakePlugin::loaded()) {
-		App::uses('PluginShortRoute', 'Routing/Route');
-		foreach ($plugins as $key => $value) {
-			$plugins[$key] = Inflector::underscore($value);
-		}
-		$pluginPattern = implode('|', $plugins);
-		$match = array('plugin' => $pluginPattern);
-		$shortParams = array('routeClass' => 'PluginShortRoute', 'plugin' => $pluginPattern);
-
-		foreach ($prefixes as $prefix) {
-			$params = array('prefix' => $prefix, $prefix => true);
-			$indexParams = $params + array('action' => 'index');
-			Router::connect("/{$prefix}/:plugin", $indexParams, $shortParams);
-			Router::connect("/{$prefix}/:plugin/:controller", $indexParams, $match);
-			Router::connect("/{$prefix}/:plugin/:controller/:action/*", $params, $match);
-		}
-		Router::connect('/:plugin', array('action' => 'index'), $shortParams);
-		Router::connect('/:plugin/:controller', array('action' => 'index'), $match);
-		Router::connect('/:plugin/:controller/:action/*', array(), $match);
-	}
-
-	foreach ($prefixes as $prefix) {
-		$params = array('prefix' => $prefix, $prefix => true);
-		$indexParams = $params + array('action' => 'index');
-		Router::connect("/{$prefix}/:controller", $indexParams);
-		Router::connect("/{$prefix}/:controller/:action/*", $params);
-	}
-	Router::connect('/:controller', array('action' => 'index'));
-	Router::connect('/:controller/:action/*');
-
-	$namedConfig = Router::namedConfig();
-	if ($namedConfig['rules'] === false) {
-		Router::connectNamed(true);
-	}
-
-	unset($namedConfig, $params, $indexParams, $prefix, $prefixes, $shortParams, $match,
-		$pluginPattern, $plugins, $key, $value);
+/**
+ * Load the CakePHP default routes. Remove this if you do not want to use
+ * the built-in default routes.
+ */
+	require CAKE . 'Config' . DS . 'routes.php';

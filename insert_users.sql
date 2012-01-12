5a9bd5036a9917448107b29989142c48fcbce8a3
Thanos Demertzis <thandem88@gmail.com>
2012-01-12 11:36:39 +0200

    add inserts

---
+++ insert_users.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: 
+-- Date: 2012-01-12 11:10
+*/
+INSERT INTO `users` (`id`,`username`,`password`,`email`,`is_banned`,`role`) VALUES (1,'user1','8f9bc2b8007a93584efdf303b83619f1fc147016','karetsosjr@gmail.com',false,'student');
+INSERT INTO `users` (`id`,`username`,`password`,`email`,`is_banned`,`role`) VALUES (2,'user2','8f9bc2b8007a93584efdf303b83619f1fc147016','thandem88@gmail.com',false,'student');
+INSERT INTO `users` (`id`,`username`,`password`,`email`,`is_banned`,`role`) VALUES (3,'user3','8f9bc2b8007a93584efdf303b83619f1fc147016','tlatsas@gmail.com',false,'student');
+INSERT INTO `users` (`id`,`username`,`password`,`email`,`is_banned`,`role`) VALUES (4,'admin','8f9bc2b8007a93584efdf303b83619f1fc147016','admin@admin.com',false,'admin');
+INSERT INTO `users` (`id`,`username`,`password`,`email`,`is_banned`,`role`) VALUES (5,'comp1','8f9bc2b8007a93584efdf303b83619f1fc147016','epix@epix.com',false,'company');

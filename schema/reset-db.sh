46c89d917bd2acbb96ff323629020ff2f37aa7bf
Thanasis Karetsos <karetsosjr@gmail.com>
2012-01-12 10:46:02 +0200

    add script for db creation and insertion

---
+++ schema/reset-db.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+dbfile="opendeals.sql"
+dbname="opendeals"
+declare -a data
+declare -a samples
+
+data=( "insert_categories.sql" "insert_offer_types.sql" )
+#samples=( "insert-sample-users.sql" "insert-sample-profiles.sql" "insert-sample-houses.sql" )
+samples=()
+
+echo -n "Enter your mysql user to connecto to db (enter for root) > "
+read dbuser
+if [[ -z $dbuser ]]; then
+    dbuser="root"
+fi
+
+echo -n "Enter your mysql password for user ${dbuser} > "
+stty -echo
+read dbpass
+stty echo
+echo -e "\n"
+
+echo -n ">> Inserting database schema..."
+mysql -u${dbuser} -p${dbpass} < ${dbfile}
+echo "DONE"
+
+echo -n ">> Inserting database data..."
+for i in ${data[@]}; do
+    mysql -u${dbuser} -p${dbpass} ${dbname} < "${i}"
+done
+echo "DONE"
+
+echo -n ">> Inserting sample data..."
+for i in ${samples[@]}; do
+    mysql -u${dbuser} -p${dbpass} ${dbname} < "${i}"
+done
+echo "DONE"

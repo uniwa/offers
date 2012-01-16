26409d4131a1c8c983d4193cc222c9fbf1003418
Thanasis Karetsos <karetsosjr@gmail.com>
2012-01-16 15:41:55 +0200

    add hash generator function in AppController

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -3,7 +3,7 @@
 class AppController extends Controller{
 
     public $components = array(
-        
+
         'Session',
         'Auth' => array(
             'authenticate' => array(
@@ -14,4 +14,14 @@ class AppController extends Controller{
         )
     );
 
+    function generateHash($size = 32) {
+
+        $size = $size - 1;
+        if ($size > 31) $size = 31;
+
+        $hash = md5(date('c'));
+        $hash = substr($hash, 0, $size);
+
+        return $hash;
+    }
 }

481dff7799ce16ac1c72924441d31ac02d735f57
Thanasis Karetsos <karetsosjr@gmail.com>
2012-01-16 12:16:38 +0200

    find the available Offer Type & Category for html select

--- app/Controller/OffersController.php
+++ app/Controller/OffersController.php
@@ -6,6 +6,7 @@ class OffersController extends AppController {
     public $helpers = array('Form');
 
     public function add() {
-
+        $this->set('offerTypes', $this->Offer->OfferType->find('list'));
+        $this->set('offerCategories', $this->Offer->OfferCategory->find('list'));
     }
 }

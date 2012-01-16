8c61776c5069d5d59b4a8059482e2061997ab586
Thanasis Karetsos <karetsosjr@gmail.com>
2012-01-16 15:49:54 +0200

    fix the size of generated hash (AppController)

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -16,8 +16,7 @@ class AppController extends Controller{
 
     function generateHash($size = 32) {
 
-        $size = $size - 1;
-        if ($size > 31) $size = 31;
+        if ($size > 32) $size = 32;
 
         $hash = md5(date('c'));
         $hash = substr($hash, 0, $size);

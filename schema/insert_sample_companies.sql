c5d6a955583b73c884b106d69c2b6d9409e6fc3c
Thanasis Karetsos <karetsosjr@gmail.com>
2012-01-18 10:55:21 +0200

    modify sample insertions to reflect the schema changes

--- schema/insert_sample_companies.sql
+++ schema/insert_sample_companies.sql
@@ -1,7 +1,7 @@
-insert into `companies` (`id`, `name`, `logo`, `address`, `postalcode`,
+insert into `companies` (`id`, `name`, `address`, `postalcode`,
                          `phone`, `fax`, `service_type`, `afm`, `doy`,
                          `working_hours`, `longitude`, `latitude`, 
                          `is_enabled`, `user_id`)
-       values (1, 'company_test_1', 'dummy blog stuffz', 'test address 28', '12345',
+       values (1, 'company_test_1', 'test address 28', '12345',
                '1234567890', '0987654321', 'estiatorio', '918273645', 'doy_aigalew',
                '13:00 - 23:00', null, null, true, 5);
--- schema/insert_sample_students.sql
+++ schema/insert_sample_students.sql
@@ -1,15 +1,15 @@
 insert into students (`id`, `firstname`, `lastname`, `receive_email`,
-                      `token`, `avatar`, `user_id`)
+                      `token`, `user_id`)
        values (1, 'student1', 'mathitidis', false,
-               null, null, 1);
+               null, 1);
 
 insert into students (`id`, `firstname`, `lastname`, `receive_email`,
-                      `token`, `avatar`, `user_id`)
+                      `token`, `user_id`)
        values (2, 'student2', 'agrammatidis', false,
-               null, null, 2);
+               null, 2);
 
 insert into students (`id`, `firstname`, `lastname`, `receive_email`,
-                      `token`, `avatar`, `user_id`)
+                      `token`, `user_id`)
        values (3, 'student3', 'nerdidis', false,
-               null, null, 3);
+               null, 3);
 

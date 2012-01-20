5b28d48b63870cd1c982dfb93c3ed92b282f3a98
Thanos Demertzis <thandem88@gmail.com>
2012-01-20 11:55:44 +0200

    insert days

---
+++ schema/insert_days.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: 
+-- Date: 2012-01-20 09:57
+*/
+INSERT INTO `days` (`id`,`name`) VALUES (,'Δευτέρα');
+INSERT INTO `days` (`id`,`name`) VALUES (NULL,'Τρίτη');
+INSERT INTO `days` (`id`,`name`) VALUES (NULL,'Τετάρτη');
+INSERT INTO `days` (`id`,`name`) VALUES (NULL,'Πέμπτη');
+INSERT INTO `days` (`id`,`name`) VALUES (NULL,'Παρασκευή');
+INSERT INTO `days` (`id`,`name`) VALUES (NULL,'Σάββατο');
+INSERT INTO `days` (`id`,`name`) VALUES (NULL,'Κυριακή');

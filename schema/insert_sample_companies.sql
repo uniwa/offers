08e5007690d45b6662833afa39ed059b2667c5ed
Thanos Demertzis <thandem88@gmail.com>
2012-01-24 09:43:19 +0200

    Database fixes

--- schema/insert_sample_companies.sql
+++ schema/insert_sample_companies.sql
@@ -1,7 +1,7 @@
 insert into `companies` (`id`, `name`, `address`, `postalcode`,
                          `phone`, `fax`, `service_type`, `afm`, `doy`,
-                         `working_hours`, `longitude`, `latitude`, 
+                          `longitude`, `latitude`, 
                          `is_enabled`, `user_id`)
        values (1, 'company_test_1', 'test address 28', '12345',
                '1234567890', '0987654321', 'estiatorio', '918273645', 'doy_aigalew',
-               '13:00 - 23:00', null, null, true, 5);
+                null, null, true, 5);

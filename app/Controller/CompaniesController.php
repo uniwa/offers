a0b7820841be93c1edab746f16ac93f5253c4bb6
Thanasis Karetsos <karetsosjr@gmail.com>
2012-01-27 12:00:51 +0200

    add logic in CompaniesController to categorize offers
    - there are 3 categories of offers:
      1) Active
      2) Draft
      3) Inactive
    - Offers are sorted by starting datetime for each
      category

--- app/Controller/CompaniesController.php
+++ app/Controller/CompaniesController.php
@@ -4,6 +4,7 @@ class CompaniesController extends AppController {
 
     public $name = 'Companies';
     public $helpers = array('Html');
+    public $uses = array('Company', 'Offer');
 
     function index() {
 
@@ -21,14 +22,43 @@ class CompaniesController extends AppController {
         $options['conditions'] = array('Company.id' => $id,
                                        'Company.is_enabled' => 1,
                                        'User.is_banned' => 0);
-        $options['recursive'] = 1;
+        $options['recursive'] = 0;
         $company = $this->Company->find('first', $options);
-
-        $this->set('company', $company);
 
         if (empty($company))
             throw new NotFoundException('Η συγκεκριμένη επιχείρηση δεν
                                         βρέθηκε.');
-// pr($company);die();
+
+        // find the active offers of this company
+        $active_options['conditions'] = array('Offer.company_id' => $id,
+                                              'Offer.is_active' => 1);
+        $active_options['fields'] = array('Offer.*');
+        $active_options['order'] = array();
+        $active_options['recrsive'] = 0;
+        $offers_tmp = $this->Offer->find('all', $active_options);
+        $offers['Offer']['Active'] = Set::extract('/Offer/.', $offers_tmp);
+
+        // find the draft offers of this company
+        $draft_options['conditions'] = array('Offer.company_id' => $id,
+                                             'Offer.is_draft' => 1);
+        $draft_options['fields'] = array('Offer.*');
+        $draft_options['order'] = array('Offer.starting ASC');
+        $draft_options['recursive'] = 0;
+        $offers_tmp = $this->Offer->find('all', $draft_options);
+        $offers['Offer']['Draft'] = Set::extract('/Offer/.', $offers_tmp);
+
+        // find the inactive offers of this company
+        $inactive_options['conditions'] = array('Offer.company_id' => $id,
+                                                'Offer.is_draft' => 0,
+                                                'Offer.is_active' => 0);
+        $inactive_options['fields'] = array('Offer.*');
+        $inactive_options['order'] = array('Offer.starting ASC');
+        $inactive_options['recursive'] = 0;
+        $offers_tmp = $this->Offer->find('all', $inactive_options);
+        $offers['Offer']['Inactive'] = Set::extract('/Offer/.', $offers_tmp);
+
+        $company = array_merge($company, $offers);
+        $this->set('company', $company);
+// pr($company); die();
     }
 }

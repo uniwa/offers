8ff4f26ff30a24783f8cceaccee8af0db2281bc1
Thanasis Karetsos <karetsosjr@gmail.com>
2012-02-14 11:54:23 +0200

    add municipality to company sample (insertion script)

--- schema/insert_sample_companies.sql
+++ schema/insert_sample_companies.sql
@@ -1,7 +1,7 @@
 insert into `companies` (`id`, `name`, `address`, `postalcode`,
                          `phone`, `fax`, `service_type`, `afm`,
                           `longitude`, `latitude`, 
-                         `is_enabled`, `user_id`)
+                         `is_enabled`, `user_id`, `municipality_id`)
        values (1, 'company_test_1', 'test address 28', '12345',
                '1234567890', '0987654321', 'estiatorio', '918273645',
-                null, null, true, 5);
+                null, null, true, 5, 13);

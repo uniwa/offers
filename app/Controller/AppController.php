c4525daa812ad2b03298e0de19ae487a91aa83e4
Thanasis Karetsos <karetsosjr@gmail.com>
2012-02-15 12:34:27 +0200

    refactor processImages method (fat one)
    - added an extra parameter in order to handle possible foreign keys
    - updated documentation
    - improved readability (a lot!)

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -22,7 +22,7 @@ class AppController extends Controller{
     }
 
 
-    function generateHash($size = 32) {
+    protected function generateHash($size = 32) {
 
         return substr(md5(date('c')), 0, $size > 32 ? 32 : $size);
     }
@@ -40,75 +40,64 @@ class AppController extends Controller{
 
     /**
      * @short Properly modify image arrays so as to save them in the database
-     * Takes an image or array of images as Form::input() (option 'type' => 'file')
-     * helper function returns them and modifies it so as to save it in
-     * opendeals.images table. If the given images already exist in the table
-     * then nothing is done and an empty array is returned.
+     * Takes an image or array of images (as returned by Form::input() helper)
+     * and modifies it so as to save it in opendeals.images table.
+     * Note: already existing images are _not_ duplicated
      *
      * @param $images The array of images, or image
      * @param $image_category The category of the image according to table opendeals.image_categories
+     * @param $foreign_keys Key => Value array containing required foreign key values
      *
      * @throws ImageExtensionException
      * @throws UploadFileException
      */
-    protected function processImages($images, $image_category = 1) {
-
-        if (isset($images['tmp_name'])) {
+    protected function processImages($images,
+                                     $image_category = 1,
+                                     $foreign_keys = array())
+    {
+        if (isset($images['tmp_name']))
             // if one image
-            $tmp = $this->_processImage($images, $image_category);
-            if (empty($tmp))
-                return $tmp;
-            else
-                return $tmp['Image'];
-        } else {
+            return $this->_processImage($images, $image_category, $foreign_keys);
+
+        else {
             // if many images
             $photos = array();
             foreach ($images as $image) {
-                $tmp = $this->_processImage($image, $image_category);
-                if (!empty($tmp))
-                    array_push($photos, $this->_processImage($image, $image_category));
+                $tmp = $this->_processImage($image, $image_category, $foreign_keys);
+                if (!empty($tmp)) $photos[] = $tmp;
             }
-            if (empty($photos))
-                return $photos;
-            else
-                return Set::extract('/Image/.', $photos);
+            return $photos;
         }
     }
 
     /**
      * @short helper function that sets the required image information
-     * Creates the blob (base64 encoded) and all the other required
-     * information about the image.
+     * Creates the blob (base64 encoded) and sets all the required
+     * information about the image in the returned array.
      *
      * @param $image The image to process
      * @param $image_category The category of image
+     * @param $foreign_keys Key => Value array containing required foreign key values
      *
      * @throws ImageExtensionException
      * @throws UploadFileException
      */
-    private function _processImage($image, $image_category) {
-        if (isset($image['tmp_name']) && $image['tmp_name'] != null) {
-            $photo = array();
-            // if image already exists in DB then return empty array
-            if (isset($image['id'])) return $photo;
+    private function _processImage($image, $image_category, $foreign_keys) {
+        if (!isset($image['tmp_name']) || isset($image['id'])) return array();
 
-            if (is_uploaded_file($image['tmp_name'])) {
-                // check validity of image
-                if ($this->isImage($image['type'])) {
-                    $file = fread(fopen($image['tmp_name'], 'r'),
-                                    $image['size']);
-                    $photo['Image'] = $image;
-                    $photo['Image']['data'] = base64_encode($file);
-                    $photo['Image']['image_category_id'] = $image_category;
-                    return $photo;
-                } else {
-                    throw new ImageExtensionException();
-                }
-            } else {
-                throw new UploadFileException();
-            }
-        } else {
-            return array();
-        }
+        if (!is_uploaded_file($image['tmp_name']))
+            throw new UploadFileException();
+
+        if (!$this->isImage($image['type']))
+            throw new ImageExtensionException();
+
+        $file = fread(fopen($image['tmp_name'], 'r'), $image['size']);
+        $image['data'] = base64_encode($file);
+        $image['image_category_id'] = $image_category;
+        // set the foreign keys if needed
+        foreach (array_keys($foreign_keys) as $key)
+            $image[$key] = $foreign_keys[$key];
+
+        return $image;
     }
 }

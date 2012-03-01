8aedc352d6c0fcaf6e391db9eb2cd31242ac2602
Thanasis Karetsos <karetsosjr@gmail.com>
2012-03-01 15:12:12 +0200

    add offer_states insertion script

---
+++ schema/insert_offer_states.sql
@@ -0,0 +1,3 @@
+insert into `offer_states` (`id`, `name`) values (null, 'draft');
+insert into `offer_states` (`id`, `name`) values (null, 'active');
+insert into `offer_states` (`id`, `name`) values (null, 'inactive');

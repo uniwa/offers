2301a8015c50de407c8863a56c0e6d651ddafb29
Thanos Demertzis <thandem88@gmail.com>
2012-03-05 11:36:08 +0200

    fix login form to remain open on click

--- app/webroot/js/global.js
+++ app/webroot/js/global.js
@@ -1,4 +1,9 @@
 $(function() { 
-	
-	$('.dropdown-toggle').dropdown();
+
+    $('.dropdown-toggle').dropdown();
+    
+    //Stop Dropdown closing when i click on it.
+    $('.dropdown-menu').find('form').click(function (e) {
+            e.stopPropagation();
+    });
 });

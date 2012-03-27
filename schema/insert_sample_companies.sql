a29ddda61e58e77bb424d0bbd965ce113ee1206c
Thanasis Karetsos <karetsosjr@gmail.com>
2012-03-27 11:55:58 +0300

    set valid AFM in sample company insertion script

--- schema/insert_sample_companies.sql
+++ schema/insert_sample_companies.sql
@@ -3,5 +3,5 @@ insert into `companies` (`id`, `name`, `address`, `postalcode`,
                           `longitude`, `latitude`, 
                          `is_enabled`, `user_id`, `municipality_id`)
        values (1, 'company_test_1', 'test address 28', '12345',
-               '1234567890', '0987654321', 'estiatorio', '918273645',
+               '1234567890', '0987654321', 'estiatorio', '011111111',
                 null, null, true, 5, 13);

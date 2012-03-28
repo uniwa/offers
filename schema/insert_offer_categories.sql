09c086d02f9216ffd9e29adcce53f1068fc494c9
Konstantinos Maragos <kmaragos@gmail.com>
2012-03-28 11:13:17 +0300

    change offer_categories samples to 'happy hour', 'coupons' & 'limited'

--- schema/insert_offer_categories.sql
+++ schema/insert_offer_categories.sql
@@ -1,7 +1,7 @@
 /*
--- Query: 
+-- Query:
 -- Date: 2012-01-12 09:41
 */
 INSERT INTO `offer_categories` (`id`,`name`) VALUES (NULL,'happy hour');
+INSERT INTO `offer_categories` (`id`,`name`) VALUES (NULL,'coupons');
 INSERT INTO `offer_categories` (`id`,`name`) VALUES (NULL,'limited');
-INSERT INTO `offer_categories` (`id`,`name`) VALUES (NULL,'unlimited');

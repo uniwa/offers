789fb445749068d0b28dd6ae71182988fe8337fb
Konstantinos Maragos <kmaragos@gmail.com>
2012-04-06 13:11:32 +0300

    refactor authenticate function

--- app/Controller/Component/Auth/LdapAuthenticate.php
+++ app/Controller/Component/Auth/LdapAuthenticate.php
@@ -7,57 +7,46 @@ App::uses('LdapUtil', 'Lib');
 class LdapAuthenticate extends BaseAuthenticate {
 
     public function authenticate(CakeRequest $request, CakeResponse $response) {
-
         $username = $request['data']['User']['username'];
         $password = $request['data']['User']['password'];
         $ldap = new LdapUtil();
 
-        if( $ldap->auth( $username, $password ) ) {
-
-            $user_info = $ldap->getInfo( $username );
-            return $this->saveLdapInfo( $user_info );
-
+        if ($ldap->auth($username, $password)) {
+            $user_info = $ldap->getInfo($username);
+            $user = $this->_findUser($username,$password);
+            if (empty($user)) {
+                return $this->saveLdapInfo($user_info);
+            } else {
+                return $user;
+            }
         } else {
-
             return false;
         }
-
     }
+    
+    // Override to return hash for empty password
+	protected function _password($password) {
+		return AuthComponent::password('');
+	}
 
-    private function saveLdapInfo( $data ) {
-        
+    private function saveLdapInfo($data) {
         $model = new User();
-        $user = $model->find( 'all',
-            array( 'conditions' => array( 'username' => $data['username'] ) )
-        );
 
+        $user = array(
+            'User' => array(
+                'username' => $data['username'],
+                'password' => '',
+                'email' => $data['email'],
+                'is_banned' => 0,
+                'terms_accepted' => 0,
+                'role' => 'student'),
+            'Student' => array(
+                'firstname' => $data['first_name'],
+                'lastname' => $data['last_name']));
 
-        if( empty($user) ) {
+        $model->saveAssociated($user, array('validate' => false));
+        $user['User']['id'] = $model->id;
 
-            $new_user = array(
-                'User' =>
-                array(
-                    'username' => $data['username'],
-                    'password' => '',
-                    'email' => $data['email'],
-                    'is_banned'=>0,
-                    'terms_accepted' => 0,
-                    'role' => 'student' ),
-                'Student' =>
-                    array(
-                        'firstname' => $data['first_name'],
-                        'lastname' => $data['last_name'] ) );
-
-
-            $model->saveAssociated( $new_user, array( 'validate' => false ) );
-            return $new_user['User'];
-        }
-
-        //return user data and Auth component
-        //automagicaly wite's them in Auth.User
-        //inside Session
-        return $user[0]['User'];
-        return true;
+        return $user['User'];
     }
-
 }

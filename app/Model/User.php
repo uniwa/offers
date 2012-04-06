d01ade01eba457714504a250815fb6e7371ef75d
Konstantinos Maragos <kmaragos@gmail.com>
2012-04-06 13:08:30 +0300

    do not hash password if null

--- app/Model/User.php
+++ app/Model/User.php
@@ -121,8 +121,10 @@ class User extends AppModel {
     }
 
     public function beforeSave( $options = array() ) {
+        if (!is_null($this->data['User']['password'])){
+            $this->data['User']['password'] = AuthComponent::password($this->data['User']['password']);
+        }
 
-        $this->data['User']['password'] = AuthComponent::password($this->data['User']['password']);
         return true;
     }
 

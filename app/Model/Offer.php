86a6afa2e68d79cfde4d6a63ebca4cea08cccf49
Konstantinos Maragos <kmaragos@gmail.com>
2012-04-11 10:07:41 +0300

    add validation rules for offers

--- app/Model/Offer.php
+++ app/Model/Offer.php
@@ -1,10 +1,40 @@
 <?php
 
 class Offer extends AppModel {
-
     public $name = 'Offer';
     public $belongsTo = array('Company', 'OfferType', 'OfferCategory', 'OfferState');
     public $hasMany = array('Coupon', 'Image', 'WorkHour');
 
+    public $validate = array(
+
+        'title' => array(
+            'not_empty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Παρακαλώ εισάγετε τον τίτλο.',
+                'required' => true
+            ),
+            'valid' => array(
+                'rule' => '/^[\w\dαβγδεζηθικλμνξοπρστυφχψωΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩΆάΈέΎΉήύΊίΌόΏώϊϋΐΰς,. &]+$/',
+                'allowEmpty' => true,
+                'message' => 'Η επωνυμία περιέχει έναν μη έγκυρο χαρακτήρα.'
+            )
+        ),
+
+        'description' => array(
+            'not_empty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Παρακαλώ εισάγετε περιγραφή.',
+                'required' => true
+            ),
+        ),
+
+        'tags' => array(
+            'valid' => array(
+                'rule' => '/^[\w\dαβγδεζηθικλμνξοπρστυφχψωΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩΆάΈέΎΉήύΊίΌόΏώϊϋΐΰς -_]+$/',
+                'allowEmpty' => true,
+                'message' => 'Η επωνυμία περιέχει έναν μη έγκυρο χαρακτήρα.'
+            )
+        ),
+    );
 
 }

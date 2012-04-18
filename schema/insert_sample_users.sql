960cd3d3ee967bfef984de75284c1bd3d38c69ae
Theodoros Eleftherios Panou <peth.ez@gmail.com>
2012-04-18 10:17:53 +0300

    added realistic sample data

--- schema/insert_sample_users.sql
+++ schema/insert_sample_users.sql
@@ -2,8 +2,17 @@
 -- Query: 
 -- Date: 2012-01-12 11:10
 */
-INSERT INTO `users` (`id`,`username`,`password`,`email`,`is_banned`,`role`) VALUES (1,'user1','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','karetsosjr@gmail.com',false,'student');
-INSERT INTO `users` (`id`,`username`,`password`,`email`,`is_banned`,`role`) VALUES (2,'user2','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','thandem88@gmail.com',false,'student');
-INSERT INTO `users` (`id`,`username`,`password`,`email`,`is_banned`,`role`) VALUES (3,'user3','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','tlatsas@gmail.com',false,'student');
-INSERT INTO `users` (`id`,`username`,`password`,`email`,`is_banned`,`role`) VALUES (4,'admin','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','admin@admin.com',false,'admin');
-INSERT INTO `users` (`id`,`username`,`password`,`email`,`is_banned`,`role`) VALUES (5,'comp1','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','epix@epix.com',false,'company');
+INSERT INTO `users` VALUES
+    (1,'user1','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','karetsosjr@gmail.com',0,'student',0,NULL),
+    (2,'user2','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','thandem88@gmail.com',0,'student',0,NULL),
+    (3,'user3','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','tlatsas@gmail.com',0,'student',0,NULL),
+    (4,'admin','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','admin@admin.com',0,'admin',0,'2012-04-18 06:22:48'),
+    (5,'comp1','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','epix@epix.com',0,'company',0,'2012-04-17 09:20:37'),
+    (6,'comp2','fa187a9ff9a7e4ff94dba296e27ca691c578a6d1','some@comp2.gr',0,'company',0,'2012-04-17 09:20:15'),
+    (7,'papaki','aba9723760ce23f4ba72408e07bb0243e3119d83','papaki@duck.com',0,'company',1,NULL),
+    (8,'dnna','352a64f51e72bc012d99c8aad7fee5cc9440f317','dnna@teiath.gr',0,'student',1,'2012-04-17 12:05:11'),
+    (9,'pizzafan','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','metaxas@pizzafan.gr',0,'company',1,'2012-04-17 12:48:14'),
+    (10,'fastbook','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','info@fastbook.gr',0,'company',1,'2012-04-17 11:50:10'),
+    (11,'youthtrip','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','info@youthtrip.gr',0,'company',1,'2012-04-17 11:33:59'),
+    (12,'megaron','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','info@megaron.gr',0,'company',1,'2012-04-17 12:37:57'),
+    (13,'vynis','d2519fa8217b0e44450caa12d8dab5eb773e5d6b','info@vynis.gr',0,'company',1,'2012-04-17 12:30:18');

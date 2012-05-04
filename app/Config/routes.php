463b705f677036eb78f52fc76dd05a8a81a87c5a
Konstantinos Maragos <kmaragos@gmail.com>
2012-05-04 12:15:11 +0300

    update routes for faq & terms

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -26,15 +26,15 @@
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
     Router::connect('/', array('controller' => 'Offers', 'action' => 'index', 'home'));
-
-    Router::connect('/termsofuse', array('controller'=>'TermsOfUse', 'action' => 'index') );
+    Router::connect('/faq', array('controller'=>'Users', 'action' => 'faq') );
+    Router::connect('/termsofuse', array('controller'=>'Users', 'action' => 'terms') );
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
 /**
- * Load all plugin routes.  See the CakePlugin documentation on 
+ * Load all plugin routes.  See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
  */
 	CakePlugin::routes();

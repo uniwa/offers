d199e169e5d6f240588772d150ecf7a4eeb3fa5d
Konstantinos Maragos <kmaragos@gmail.com>
2012-05-18 12:16:55 +0300

    display vote count & sum in offer view

--- app/Controller/OffersController.php
+++ app/Controller/OffersController.php
@@ -246,11 +246,15 @@ class OffersController extends AppController {
         $new_elem['label'] = "Κατηγορία";
         $new_elem['value'] = $offer['OfferCategory']['name'];
         $offer_info[] = $new_elem;
-        $vote_count = $offer['Offer']['vote_count'];
-        $vote_class = ($vote_count >= 0)?'green':'red';
-        $votes = "<span class='votes {$vote_class}'>{$vote_count}</span>";
+        $vote_sum = $offer['Offer']['vote_sum'];
+        $vote_class = ($vote_sum >= 0)?'green':'red';
+        $votes = "<span class='votes {$vote_class}'>{$vote_sum}</span>";
         $new_elem['label'] = "Άθροισμα ψήφων";
         $new_elem['value'] = $votes;
+        $offer_info[] = $new_elem;
+        $vote_count = $offer['Offer']['vote_count'];
+        $new_elem['label'] = "Σύνολο ψήφων";
+        $new_elem['value'] = $vote_count;
         $offer_info[] = $new_elem;
         $new_elem['label'] = "Λέξεις-κλειδιά";
         $new_elem['value'] = $offer['Offer']['tags'];

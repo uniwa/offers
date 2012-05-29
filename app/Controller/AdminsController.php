4f2210b281d27fcf1351f026af79893d423e690e
Theodoros Eleftherios Panou <peth.ez@gmail.com>
2012-05-29 12:50:33 +0300

    setup access control

--- app/Controller/AdminsController.php
+++ app/Controller/AdminsController.php
@@ -4,4 +4,21 @@ class AdminsController extends AppController {
 
     public $name = 'Admins';
 
+    public function beforeFilter() {
+
+        if (! $this->is_authorized($this->Auth->user()))
+            throw new ForbiddenException();
+
+        parent::beforeFilter();
+    }
+
+    public function is_authorized($user) {
+
+        // only admin may access this controller
+        if (isset($user['role']) && $user['role'] === ROLE_ADMIN) {
+            return true;
+        }
+
+        return false;
+    }
 }

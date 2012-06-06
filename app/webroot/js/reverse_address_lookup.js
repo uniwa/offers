7da6b9639053ebc844b49977155a3ad22f6c80e2
Tasos Latsas <tlatsas2000@gmail.com>
2012-06-06 13:14:22 +0300

    handle returned data inside address_lookup()

--- app/webroot/js/reverse_address_lookup.js
+++ app/webroot/js/reverse_address_lookup.js
@@ -10,12 +10,17 @@ function address_lookup(req_url, req_address) {
                 $("#ajax-status").html('<img src="../../img/ajax-loader.gif" alt="searching" />');
             },
             success: function(data) {
-                $("#ajax-status").html('<i class="icon-ok"></i>');
-                return data;
+                data = JSON.parse(data);
+                if (data.lng == null || data.lat == null) {
+                    $("#ajax-status").html('<i class="icon-warning-sign"></i>');
+                } else {
+                    $('#ajax-status').html('<i class="icon-ok"></i>');
+                    $('#comp-longitude').val(data.lng);
+                    $('#comp-latitude').val(data.lat);
+                }
             },
             error: function() {
                 $("#ajax-status").html('<i class="icon-warning-sign"></i>');
-                return {"error": "request failed"};
             }
         }
     );

74c37090ef7661df252571ace743090d7d3ea246
Konstantinos Maragos <kmaragos@gmail.com>
2012-06-08 11:15:32 +0300

    fix some funny sample data

--- schema/insert_sample_students.sql
+++ schema/insert_sample_students.sql
@@ -1,15 +1,14 @@
 insert into students (`id`, `firstname`, `lastname`, `receive_email`,
                       `token`, `user_id`)
-       values (1, 'student1', 'mathitidis', false,
+       values (1, 'Student1', 'Φοιτητής', false,
                null, 1);
 
 insert into students (`id`, `firstname`, `lastname`, `receive_email`,
                       `token`, `user_id`)
-       values (2, 'student2', 'agrammatidis', false,
+       values (2, 'Student2', 'Φοιτητής', false,
                null, 2);
 
 insert into students (`id`, `firstname`, `lastname`, `receive_email`,
                       `token`, `user_id`)
-       values (3, 'student3', 'nerdidis', false,
+       values (3, 'Student3', 'Φοιτητής', false,
                null, 3);
-

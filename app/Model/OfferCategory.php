b1fe08506ee474c6eb5672fc372d634b3ec28c4f
Theodoros Eleftherios Panou <peth.ez@gmail.com>
2012-06-13 12:25:54 +0300

    added categories name validation rule

--- app/Model/OfferCategory.php
+++ app/Model/OfferCategory.php
@@ -4,4 +4,14 @@ class OfferCategory extends AppModel {
 
     public $name = 'OfferCategory';
     public $hasMany = 'Offer';
+    public $validate = array(
+        'name' => array(
+            'not_empty' => array(
+                'rule' => 'alphaNumeric',
+                'notEmpty' => true,
+                'required' => true,
+                'message' => 'Επιτρέπονται γράμματα και αριθμοί'
+            ),
+        )
+    );
 }

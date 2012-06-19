da4007efd478252d37f64abaa9ae07c9a156dff7
Konstantinos Maragos <kmaragos@gmail.com>
2012-06-19 11:50:09 +0300

    add action for managing students & rename view -> companies

--- app/Controller/AdminsController.php
+++ app/Controller/AdminsController.php
@@ -3,7 +3,7 @@
 class AdminsController extends AppController {
 
     public $name = 'Admins';
-    public $uses = array('Company');
+    public $uses = array('Company', 'Student');
     public $helpers = array('Html');
 
     public function beforeFilter() {
@@ -24,13 +24,93 @@ class AdminsController extends AppController {
         return false;
     }
 
-    public function view($id = null) {
+    public function students($id = null) {
 
         // == force a redirect so as to display pretty options ==
         if (! empty($this->request->data)) {
             // basis of the redirection params
             $redirect = array('controller' => 'admins',
-                              'action' => 'view');
+                              'action' => 'students');
+
+            // keep current params but replace those found in POST data
+            $redirect = array_merge($redirect,
+                                    $this->request->named,
+                                    $this->request->data);
+
+            // do NOT preserve page (because results may be differect)
+            if (isset($redirect['page'])) unset($redirect['page']);
+
+            // remove empty filters from uri
+            $redirect = array_filter($redirect);
+
+            $this->redirect($redirect);
+        }
+
+        // POST data have been moved to uri as 'pretty' params
+        $request = $this->request->named;
+
+        // == create filter conditions ==
+        // will be used to contain filtering options
+        $options = array();
+
+        if (isset($request['search'])) {
+
+            $search = $request['search'];
+            if (! empty($search)) {
+                $search_keyword = "%{$request['search']}%";
+                $or_clause = array();
+                $or_clause[] = array('Student.name LIKE' => $search_keyword);
+                $or_clause[] = array('User.username LIKE' => $search_keyword);
+                $or_clause[] = array('User.email LIKE' => $search_keyword);
+                $options['OR'] = &$or_clause;
+            }
+        }
+
+        if (isset($request['banned'])) {
+
+            $is_banned = $request['banned'] === '1';
+            $options[] = array('User.is_banned' => $is_banned);
+        }
+
+        // == apply pagination options ==
+        //TODO: place this somewhere else
+        $pagination_limit = 10;
+
+        // let's be lightweight - fetch only required attributes
+        $fields = array('Student.id',
+                        'Student.name',
+                        'User.username',
+                        'User.email',
+                        'User.is_banned');
+
+        // basic pagination options
+        $pagination_options = array('limit' => $pagination_limit,
+                                    'fields' => $fields,
+                                    'recursive' => 0);
+
+        // append filter conditions, if available
+        if (! empty($options))
+            $pagination_options['conditions'] = $options;
+
+        $this->paginate = $pagination_options;
+
+        $this->Student->Behaviors->attach('Containable');
+        $this->Student->contain(array('User'));
+        $data = $this->paginate('Student');
+
+        // data to display
+        $this->set('data', $data);
+        // search options with which to fill-in the form
+        $this->set('request', $request);
+    }
+
+    public function companies($id = null) {
+
+        // == force a redirect so as to display pretty options ==
+        if (! empty($this->request->data)) {
+            // basis of the redirection params
+            $redirect = array('controller' => 'admins',
+                              'action' => 'companies');
 
             // keep current params but replace those found in POST data
             $redirect = array_merge($redirect,

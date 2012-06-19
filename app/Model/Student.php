1be862a2089b1f17bad65834645adbe25d08d169
Konstantinos Maragos <kmaragos@gmail.com>
2012-06-19 11:50:52 +0300

    add virtual field name = 'first last' in Student model

--- app/Model/Student.php
+++ app/Model/Student.php
@@ -5,4 +5,6 @@ class Student extends AppModel {
     public $name = 'Student';
     public $belongsTo = array('User');
     public $hasMany = 'Coupons';
+    public $virtualFields = array(
+        'name' => 'CONCAT(Student.firstname, " ", Student.lastname)');
 }

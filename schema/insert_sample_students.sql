dc2b6cf085f7a66f7f4b82b2d6f8919e2b74bd85
Konstantinos Maragos <kmaragos@gmail.com>
2012-06-19 12:30:24 +0300

    update sample inserts

--- schema/insert_sample_students.sql
+++ schema/insert_sample_students.sql
@@ -1,14 +1,5 @@
-insert into students (`id`, `firstname`, `lastname`, `receive_email`,
-                      `token`, `user_id`)
-       values (1, 'Student1', 'Φοιτητής', false,
-               null, 1);
-
-insert into students (`id`, `firstname`, `lastname`, `receive_email`,
-                      `token`, `user_id`)
-       values (2, 'Student2', 'Φοιτητής', false,
-               null, 2);
-
-insert into students (`id`, `firstname`, `lastname`, `receive_email`,
-                      `token`, `user_id`)
-       values (3, 'Student3', 'Φοιτητής', false,
-               null, 3);
+INSERT INTO `students` (`id`, `firstname`, `lastname`, `receive_email`, `user_id`)
+VALUES
+(1, 'Student1', 'Φοιτητής', false, 1),
+(2, 'Student2', 'Φοιτητής', false, 2),
+(3, 'Student3', 'Φοιτητής', false, 3);

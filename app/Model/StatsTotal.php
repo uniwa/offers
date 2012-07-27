37e7fd9c6945020bd762e800322c53ca0e5cd6f3
Konstantinos Maragos <kmaragos@gmail.com>
2012-07-27 13:18:53 +0300

    add function to record visit statistics for a single date

--- app/Model/StatsTotal.php
+++ app/Model/StatsTotal.php
@@ -3,6 +3,18 @@
 class StatsTotal extends AppModel {
 
     public $name = 'StatsTotal';
+    public $useTable = 'stats_total';
     public $belongsTo = array('Offer', 'Company');
+
+    // Create and add a new record containing statistics for a single day
+    // $data should be an array containing the following key/value pairs:
+    // oid: offer_id
+    // cid: company_id
+    // date: date of stats
+    // total: total visits on given date
+    // unique: unique visitors on given date
+    public function add_daily_stats($data) {
+        return $this->save($data, false);
+    }
 
 }

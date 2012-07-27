5e572ac089800bce800eab18f21db8166c865477
Konstantinos Maragos <kmaragos@gmail.com>
2012-07-27 13:18:08 +0300

    add function to record visit data

--- app/Model/StatsToday.php
+++ app/Model/StatsToday.php
@@ -3,6 +3,16 @@
 class StatsToday extends AppModel {
 
     public $name = 'StatsToday';
+    public $useTable = 'stats_today';
     public $belongsTo = array('Offer', 'Company');
+
+    // Create and add a new record containing statistics for a single visit
+    // $data should be an array containing the following key/value pairs:
+    // oid: offer_id
+    // cid: company_id
+    // ip: IP address of visitor
+    public function add_visit($data) {
+        return $this->save($data, false);
+    }
 
 }

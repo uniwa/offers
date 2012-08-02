adc98f501b91999e687b3e3cd5a91713976a6dc8
Theodoros Eleftherios Panou <peth.ez@gmail.com>
2012-08-02 10:46:23 +0300

    coupon method to fetch serial number listing of offer

--- app/Model/Coupon.php
+++ app/Model/Coupon.php
@@ -59,4 +59,22 @@ class Coupon extends AppModel {
         $result = $this->find('first', $options);
         return $result['Offer']['company_id'] === $company_id;
     }
+
+    // Returns an array of coupon serial numbers of the specified offer.
+    // All coupons are included except for those that have been marked as
+    // 'reinserted'.
+    //
+    // No custom find method is created instead, because it would require to
+    // specify the offer id via an array (as param), i.e. clutter; the reason
+    // for defining this method is to avoid clutter.
+    public function get_offer_coupons($offer_id = null) {
+        if (empty($offer_id)) return false;
+
+        $options = array(
+                'conditions' => array('Coupon.offer_id' => $offer_id,
+                                      'Coupon.reinserted' => false),
+                'fields' => array('Coupon.serial_number'));
+
+        return $this->find('all', $options);
+    }
 }

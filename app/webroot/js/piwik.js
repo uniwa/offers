63a72742f0de888e1707d6a35c684d47beb9d3ca
Tasos Latsas <tlatsas2000@gmail.com>
2012-10-15 12:01:13 +0300

    Js bugfix

--- app/webroot/js/piwik.js
+++ app/webroot/js/piwik.js
@@ -1,8 +1,7 @@
 var pkBaseURL = (("https:" == document.location.protocol) ? "https://livestats.noc.teiath.gr/piwik/" : "http://livestats.noc.teiath.gr/piwik/");
 document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-</script><script type="text/javascript">
 try {
-var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 13);
-piwikTracker.trackPageView();
-piwikTracker.enableLinkTracking();
+    var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 13);
+    piwikTracker.trackPageView();
+    piwikTracker.enableLinkTracking();
 } catch( err ) {}
